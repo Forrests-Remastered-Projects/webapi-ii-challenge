26d932ed97e02f1b6d42c5b40a73a4c76354bfc4
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-29 14:21:53 -0700

    edit/put and gt by id are done, now working on delete

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -32,4 +32,42 @@ router.post("/", (req, res) => {
       res.status(500).json({ error: "Error getting post" });
     });
 });
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db.findById(id, res).then(post => {
+    console.log(post);
+    if (post.length) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ error: "Post with this ID does not exist" });
+    }
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+  if (!title && !contents) {
+    return res
+      .status(400)
+      .json({ error: "Error, must include title and contents" });
+  }
+  console.log(req.body);
+  db.update(id, { title, contents })
+    .then(updated => {
+      if (updated === 1) {
+        db.findById(id).then(post => {
+          res.status(200).json(post);
+        });
+        // res.status().json({ error: "Error, must add changes" });
+        console.log(updated);
+      } else {
+        res.status(404).json({ error: "Post with id does not exist" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Error updating post" });
+    });
+});
 module.exports = router;
