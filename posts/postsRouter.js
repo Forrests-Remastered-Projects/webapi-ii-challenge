9912c7ba007b407d8fa6351c2d27acf0ca3f3baa
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-29 15:33:39 -0700

    Posting comments done, mvp met

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -103,5 +103,29 @@ router.get("/:post_id/comments", (req, res) => {
       console.log("get comments", err);
       res.status(500).json({ error: "Error getting post comments" });
     });
+  router.post("/:post_id/comments", (req, res) => {
+    const { post_id } = req.params;
+    const { text } = req.body;
+
+    db.insertComment({ text, post_id })
+      .then(({ id: comment_id }) => {
+        db.findCommentById(comment_id)
+          .then(([comment]) => {
+            if (comment) {
+              res.status(200).json(comment);
+            } else {
+              res.status(404).json({ error: "Comment with id not found" });
+            }
+          })
+          .catch(err => {
+            console.log("post comment get", err);
+            res.status(500).json({ error: "Error getting comment" });
+          });
+      })
+      .catch(err => {
+        console.log("post comment", err);
+        res.status(500).json({ error: "Error adding comment" });
+      });
+  });
 });
 module.exports = router;
